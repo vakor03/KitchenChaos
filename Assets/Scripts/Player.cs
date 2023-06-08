2e126943e43126849655a8b5fe5abb564ce0ec6b
valerii.korniienko <vakor.dev@gmail.com>
2023-06-08 14:39:54 +0300

    CollisionDetections

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,9 +15,59 @@ public class Player : MonoBehaviour
         _isWalking = inputVector != Vector2.zero;
 
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDirection * (Time.deltaTime * speed);
+
+        float moveDistance = speed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+
+        var canMove = TryMoveInDirection(playerHeight, playerRadius, moveDistance, ref moveDirection);
+
+        if (canMove)
+        {
+            transform.position += moveDirection * moveDistance;
+        }
+
 
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+    }
+
+    private bool TryMoveInDirection(float playerHeight, float playerRadius, float moveDistance,
+        ref Vector3 moveDirection)
+    {
+        bool canMove = !Physics.CapsuleCast(transform.position,
+            transform.position + Vector3.up * playerHeight,
+            playerRadius,
+            moveDirection,
+            moveDistance);
+
+        if (!canMove)
+        {
+            //Try move in X direction
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
+            if (!Physics.CapsuleCast(transform.position,
+                    transform.position + Vector3.up * playerHeight,
+                    playerRadius,
+                    moveDirectionX,
+                    moveDistance))
+            {
+                moveDirection = moveDirectionX;
+                return true;
+            }
+
+            Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
+
+            if (!Physics.CapsuleCast(transform.position,
+                    transform.position + Vector3.up * playerHeight,
+                    playerRadius,
+                    moveDirectionZ,
+                    moveDistance))
+            {
+                moveDirection = moveDirectionZ;
+                return true;
+            }
+        }
+
+        return canMove;
     }
 
     public bool IsWalking()
