563a1281afac50a9f96919c7aad39dd393887568
valerii.korniienko <vakor.dev@gmail.com>
2023-06-08 13:11:04 +0300

    Basic PlayerAnimator

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ public class Player : MonoBehaviour
     [SerializeField] private float speed = 7f;
     [SerializeField] private float rotateSpeed = 10f;
 
+    private bool _isWalking;
+
     private void Update()
     {
         Vector2 inputVector = Vector2.zero;
@@ -31,10 +33,16 @@ public class Player : MonoBehaviour
         }
 
         inputVector = inputVector.normalized;
+        _isWalking = inputVector != Vector2.zero;
 
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDirection * (Time.deltaTime * speed);
 
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return _isWalking;
     }
 }
