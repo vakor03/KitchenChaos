ef3b76c803ce4e621824bb6524b86ae75d7d9e1e
valerii.korniienko <vakor.dev@gmail.com>
2023-06-08 15:23:51 +0300

    Basic Raycast interactions

---
+++ Assets/Scripts/ClearCounter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class ClearCounter : MonoBehaviour
+    {
+        public void Interact()
+        {
+            Debug.Log("Interact");
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,15 +6,44 @@ public class Player : MonoBehaviour
     [SerializeField] private float speed = 7f;
     [SerializeField] private float rotateSpeed = 10f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private float interactDistance = 2f;
+    [SerializeField] private LayerMask interactionLayerMask;
 
     private bool _isWalking;
+    private Vector3 _lastInteractDirection;
 
     private void Update()
     {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    private void HandleInteractions()
+    {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        _isWalking = inputVector != Vector2.zero;
 
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDirection != Vector3.zero)
+        {
+            _lastInteractDirection = moveDirection;
+        }
+
+        bool isRaycastHit = Physics.Raycast(transform.position, _lastInteractDirection, 
+            out RaycastHit raycastHit, interactDistance,interactionLayerMask);
+        
+        if (isRaycastHit && raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+        {
+            clearCounter.Interact();
+        }
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+        _isWalking = inputVector != Vector2.zero;
 
         float moveDistance = speed * Time.deltaTime;
         float playerRadius = 0.7f;
@@ -42,7 +71,6 @@ public class Player : MonoBehaviour
 
         if (!canMove)
         {
-            //Try move in X direction
             Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
             if (!Physics.CapsuleCast(transform.position,
                     transform.position + Vector3.up * playerHeight,
