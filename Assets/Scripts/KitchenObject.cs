5b8d1052cc65df2f021e842d283e39e4b3f37fe7
valerii.korniienko <vakor.dev@gmail.com>
2023-06-09 19:24:59 +0300

    Added KitchenObjects

---
+++ Assets/Scripts/KitchenObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class KitchenObject : MonoBehaviour
+{
+    [field: SerializeField] public KitchenObjectSO KitchenObjectSO { get; private set; }
+
+    // public ClearCounter ClearCounter
+    // {
+    //     get => _clearCounter;
+    //     set
+    //     {
+    //         
+    //     }
+    // }
+
+    private ClearCounter _clearCounter;
+}
