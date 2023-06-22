cb98b83dd71926ba77592eb4ddab42928cf945c2
valerii.korniienko <vakor.dev@gmail.com>
2023-06-22 16:42:29 +0300

    MainMenu Scene

---
+++ Assets/Scripts/Core/LoaderCallback.cs
@@ -0,0 +1,22 @@
+﻿#region
+
+using UnityEngine;
+
+#endregion
+
+namespace Core
+{
+    public class LoaderCallback : MonoBehaviour
+    {
+        private bool _firstUpdate = true;
+
+        private void Update()
+        {
+            if (_firstUpdate)
+            {
+                Loader.LoaderCallback();
+                _firstUpdate = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Core/MainMenuUI.cs
@@ -0,0 +1,21 @@
+﻿#region
+
+using UnityEngine;
+using UnityEngine.UI;
+
+#endregion
+
+namespace Core
+{
+    public class MainMenuUI : MonoBehaviour
+    {
+        [SerializeField] private Button playButton;
+        [SerializeField] private Button quitButton;
+
+        private void Awake()
+        {
+            playButton.onClick.AddListener(() => { Loader.LoadScene(Loader.Scene.GameScene); });
+            quitButton.onClick.AddListener(Application.Quit);
+        }
+    }
+}
